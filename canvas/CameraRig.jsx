26060a761bbaedb8856ccd33962ce975cc869fa1
Yasmine <yasmineharfouche0@gmail.com>
2025-09-04 10:55:09 +0200

    Add files via upload

---
+++ canvas/CameraRig.jsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { easing } from 'maath'
+import state from '../canvas/state'
+import { useSnapshot } from 'valtio'
+
+const CameraRig = ({ children }) => {
+    const group = useRef()
+    const snap = useSnapshot(state)
+  
+    useFrame((state, delta) => { // Ajoutez delta comme deuxième paramètre
+      const isBreakpoint = window.innerWidth <= 1260
+      const isMobile = window.innerWidth <= 600
+  
+      let targetPosition = [0, 0.2, 2] // Position plus neutre
+      
+      if (snap.intro) {
+        if (isBreakpoint) targetPosition = [0, 0.2, 2.5]
+        if (isMobile) targetPosition = [0, 0.2, 3]
+      } else {
+        if (isMobile) targetPosition = [0, 0, 2.5]
+        else targetPosition = [0, 0, 2]
+      }
+  
+      easing.damp3(state.camera.position, targetPosition, 0.25, delta) // Maintenant delta est défini
+  
+      // Rotation simplifiée
+      easing.dampE(
+        group.current.rotation,
+        [state.pointer.y / 10, -state.pointer.x / 5, 0],
+        0.25,
+        delta
+      )
+    })
+  
+    return <group ref={group}>{children}</group>
+  }
+  export default CameraRig
---
+++ canvas/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dress from './Shirt'
+
+
+
+const CanvasModel = () => {
+  return (
+    
+      <Dress />
+    
+  )
+}
+
+export default CanvasModel
